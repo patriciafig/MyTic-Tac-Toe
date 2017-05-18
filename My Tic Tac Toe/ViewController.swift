8b64684e84c5a1be304200b734cd669b3c96a285
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-05-18 12:38:14 -0600

    Can now tell who wins, x or o based on winning combinations

--- My Tic Tac Toe/ViewController.swift
+++ My Tic Tac Toe/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
     var ActivePlayer = 1  // x
     var GameStatus = [0,0,0,0,0,0,0,0,0]   // each represent a place in the board
     
+    let  WinCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]      // defines how to classify a winner based on tag position of x or o
+    
+    var ActiveGame = true
+    
+    
     @IBAction func action(_ sender: AnyObject)   // display x or o
     {
         // check game status according to the sender tag
@@ -33,8 +38,39 @@ class ViewController: UIViewController {
                 sender.setImage(UIImage(named: "o.png"), for: UIControlState())    // allows you to switch from x or o
                 ActivePlayer = 1
             }
+        }
+        
+          // loops through all the winning combinations
+        for combinations in WinCombinations
+        {
+            if GameStatus[combinations[0]] != 0 && GameStatus[combinations[0]] == GameStatus[combinations[1]] && GameStatus[combinations[1]] == GameStatus[combinations[2]]
+            {
+            
+                ActiveGame = false     // pause game if winner 
+                if GameStatus[combinations[0]] == 1
+                {
+                 //  x has won
+                    print ("X");
+                
+                }
+                else
+                {
+                
+                // o has won
+                    
+                    print ("O");
+                
+                }
+            
+            
+            }
+            
+            
+            
         
         }
+        
+        
         
     }
     
